8a31ea4290bade4888e3708e80d2ff3fe350187a
Koichi <koichi89592@gmail.com>
2021-02-09 21:51:22 +0900

    Set setting

--- src/firebase.utils.js
+++ src/firebase.utils.js
@@ -3,3 +3,9 @@ import firebase from 'firebase';
 import firebaseConfig from '../firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account ' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
