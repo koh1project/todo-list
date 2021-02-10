fd74ddbdf49ae7ea078b1847192803e3e97bffa9
Koichi <koichi89592@gmail.com>
2021-02-10 20:20:30 +0900

    Add form email and password

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = userCredentials;
+  const handleChange = (event: any) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
+  const loginHandler = (evt: React.FormEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    console.log(userCredentials);
+  };
   return (
     <div>
-      <input type="text"></input>
-      <input type="submit" value="submit" />
+      <input
+        name="email"
+        type="text"
+        onChange={(evt) => {
+          handleChange(evt);
+        }}
+        value={email}
+      ></input>
+      <input
+        name="password"
+        type="password"
+        onChange={(evt) => {
+          handleChange(evt);
+        }}
+        value={password}
+      ></input>
+      <input type="submit" value="submit" onClick={(evt) => loginHandler(evt)} />
     </div>
   );
 };
