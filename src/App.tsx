b32409de5311cb7e7b24ae76114875a8e47ea163
Koichi <koichi89592@gmail.com>
2021-02-10 19:44:06 +0900

    Add Router and login page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
+import Login from './pages/login/login';
+
 const App = () => {
   return (
     <div className="App">
       <h1>TEST</h1>
+      <Route path={'/login'} component={Login} />
     </div>
   );
 };
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text"></input>
+      <input type="submit" value="submit" />
+    </div>
+  );
+};
+
+export default Login;
