5d3adebfa179ece97a007cc8a3c2c18a82140ca5
Koichi <koichi89592@gmail.com>
2021-02-11 19:15:29 +0900

    Add sign-in component

---
+++ src/components/sign-in/sign-in.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState } from 'react';
+import FormInput from '../form-input/form-input';
+
+const SignIn: FC = (props) => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = userCredentials;
+  const handleChange = (event: any) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
+  const loginHandler = (evt: React.FormEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    console.log(userCredentials);
+  };
+  return (
+    <div>
+      <FormInput name="email" type="text" value={email} handleChange={handleChange} label="email"></FormInput>
+      <FormInput
+        name="password"
+        type="password"
+        handleChange={handleChange}
+        value={password}
+        label="password"
+      ></FormInput>
+      <input type="submit" value="submit" onClick={(evt) => loginHandler(evt)} />
+
+      <button>google signin</button>
+
+    </div>
+  );
+};
+
+export default SignIn;
