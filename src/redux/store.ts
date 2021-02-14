0d10b0a2d6751fe116461f1f63acd99458264d8f
Koichi <koichi89592@gmail.com>
2021-02-14 20:52:26 +0900

    Set store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,19 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+import rootReducer from './root-reducer';
+
+import { Middleware } from 'redux';
+const middlewares: Middleware[] = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
+
+export default { store, persistStore };
