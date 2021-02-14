dda6570ffa1817689e9290eb6642b00aec647077
Koichi <koichi89592@gmail.com>
2021-02-14 21:17:28 +0900

    Make user.reducer.ts

---
+++ src/redux/user/user.reducer.ts
@@ -0,0 +1,21 @@
+import { Reducer } from 'redux';
+import { UserActionTypes } from './user.types';
+import { UserAction } from './user.actions';
+
+export type UserState = { currentUser: string | null };
+export const INITIAL_STATE: UserState = { currentUser: null };
+
+export const userReducer: Reducer<UserState, UserAction> = (
+  state: UserState = INITIAL_STATE,
+  action: UserAction
+): UserState => {
+  switch (action.type) {
+    case UserActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    default:
+      return state;
+  }
+};
