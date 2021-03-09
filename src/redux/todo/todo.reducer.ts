96fda002f8baa546d7be05531f15c4ccfce39632
Koichi <koichi89592@gmail.com>
2021-03-09 22:10:43 +0900

    Add previousTodos state

--- src/redux/todo/todo.reducer.ts
+++ src/redux/todo/todo.reducer.ts
@@ -3,8 +3,8 @@ import { Reducer } from 'redux';
 import { TodoActionTypes } from './todo.types';
 import { TodoAction } from './todo.actions';
 
-export type TodoState = { todos: Todo[] };
-export const INITIAL_STATE: TodoState = { todos: [] };
+export type TodoState = { todos: Todo[]; previousTodos: Todo[] };
+export const INITIAL_STATE: TodoState = { todos: [], previousTodos: [] };
 
 export const todoReducer: Reducer<TodoState, TodoAction> = (
   state: TodoState = INITIAL_STATE,
@@ -14,23 +14,26 @@ export const todoReducer: Reducer<TodoState, TodoAction> = (
     case TodoActionTypes.ADD_TODO_ITEM:
       return {
         ...state,
+        previousTodos: state.todos,
         todos: [...state.todos, action.payload as Todo]
       };
     case TodoActionTypes.DELETE_TODO_ITEM:
       return {
         ...state,
+        previousTodos: state.todos,
         todos: state.todos.slice().filter((todo) => todo.id !== (action.payload as Todo).id)
       };
     case TodoActionTypes.UPDATE_TODO_ITEM:
       const editedTodo = action.payload as Todo;
       return {
         ...state,
+        previousTodos: state.todos,
         todos: state.todos.map((todo) => (todo.id === editedTodo?.id ? editedTodo : todo))
       };
     case TodoActionTypes.FETCH_TODOS_SUCCESS:
-      return { ...state, todos: action.payload as Todo[] };
+      return { ...state, previousTodos: state.todos, todos: action.payload as Todo[] };
     case TodoActionTypes.SYNC_TODOS:
-      return { ...state, todos: action.payload as Todo[] };
+      return { ...state, previousTodos: state.todos, todos: action.payload as Todo[] };
     default:
       return state;
   }
