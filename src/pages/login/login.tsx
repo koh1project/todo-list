27d0838f80038a433a84b4e60b33788d0db03d51
Koichi <koichi89592@gmail.com>
2021-03-21 22:21:52 +0900

    Add @TODO comment

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,11 @@
 import SignIn from 'components/sign-in/sign-in';
 
 export const Login = () => {
+
+
+  // @TODO: Sign up component
+  // @TODO: Use UI component
+
   return (
     <div>
       <h1>Login page</h1>
