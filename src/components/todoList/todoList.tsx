2823dfe6bff22ea53ace2f7b62f9ab7c1153a02c
Koichi <koichi89592@gmail.com>
2021-03-24 21:12:01 +0900

    Add changing complete flg

--- src/components/todoList/todoList.tsx
+++ src/components/todoList/todoList.tsx
@@ -1,7 +1,9 @@
 import React, { VFC } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { Todo } from 'redux/todo/todo.actions';
+import { Todo, updateTodosStartAsync } from 'redux/todo/todo.actions';
 import { TodoListItem } from 'components/todoListItem/todoListItem';
+
 type TodoListProps = {
   todos: Todo[];
   userId: string;
@@ -9,11 +11,20 @@ type TodoListProps = {
 };
 
 export const TodoList: VFC<TodoListProps> = ({ label, todos, userId }) => {
+  const dispatch = useDispatch();
+  const handlerOnClick = (todo: Todo) => {
+    const changedTodo = { ...todo, done: !todo.done };
+
+    console.log(todos);
+
+    dispatch(updateTodosStartAsync(todos, changedTodo, userId));
+  };
+
   return (
     <div>
       <h2>{label}</h2>
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={Math.random()} userId={userId} onClickCheckBox={() => console.log(`Clicked`)} />
+        <TodoListItem todo={todo} key={Math.random()} userId={userId} onClickCheckBox={handlerOnClick} />
       ))}
     </div>
   );
--- src/components/todoListItem/todoListItem.tsx
+++ src/components/todoListItem/todoListItem.tsx
@@ -1,11 +1,10 @@
 import React, { VFC, useState } from 'react';
 
 import { Todo } from 'redux/todo/todo.actions';
-import { deleteTodosStartAsync } from 'redux/todo/todo.actions';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { EditableTodoListItem } from 'components/editableTodoListItem/editableTodoListItem';
 import { formattedDateString } from 'utils';
-import { RootState } from 'redux/root-reducer';
+
 type TodoListItemProps = {
   todo: Todo;
   userId: string;
@@ -14,9 +13,7 @@ type TodoListItemProps = {
 };
 
 export const TodoListItem: VFC<TodoListItemProps> = ({ todo, userId, onClickCheckBox }) => {
-  const dispatch = useDispatch();
   const [clicked, setClicked] = useState<boolean>(false);
-  const todos = useSelector((state: RootState) => state.todo.todos);
 
   const handlerClicked = (evt: React.MouseEvent) => {
     evt.preventDefault();
@@ -27,7 +24,7 @@ export const TodoListItem: VFC<TodoListItemProps> = ({ todo, userId, onClickChec
     <EditableTodoListItem clicked={setClicked} userId={userId} todo={todo} />
   ) : (
     <div key={Math.random()}>
-      <input type="checkbox" onClick={() => onClickCheckBox()} />
+      <input type="checkbox" onClick={() => onClickCheckBox(todo)} />
       <span onClick={(evt) => handlerClicked(evt)}>
         {todo.description} - {formattedDateString(todo.dueDate)}
       </span>
