4810af2a53cde26a6262095479ed0151ee208f4e
Koichi <koichi89592@gmail.com>
2021-03-28 19:23:04 +0900

    Set error handling

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -5,13 +5,15 @@ import { setCurrentUser } from '../../redux/user/user.actions';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-const SignIn: VFC = (props) => {
+const SignIn: VFC = () => {
   const dispatch = useDispatch();
   const [userCredentials, setUserCredentials] = useState({
     email: '',
     password: '',
   });
   const { email, password } = userCredentials;
+  const [error, setError] = useState('');
+
   const handleChange = (event: any) => {
     event.preventDefault();
     const { name, value } = event.target;
@@ -24,7 +26,23 @@ const SignIn: VFC = (props) => {
     evt.preventDefault();
     console.log(userCredentials);
     // TODO: Login to firebase
-    // const { user } = await auth.signInWithEmailAndPassword(email,password);
+    try {
+      const { user } = await auth.signInWithEmailAndPassword(email, password);
+
+      if (user) {
+        dispatch(setCurrentUser(user.uid));
+        history.push('/');
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  //@TODO: 制作用メソッド
+  const TestHandleSubmit = async (evt: React.FormEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+    console.log(userCredentials);
+
     const { user } = await auth.signInWithEmailAndPassword('test@gmail.com', 'aaaaaa');
     if (user) {
       dispatch(setCurrentUser(user.uid));
@@ -34,6 +52,7 @@ const SignIn: VFC = (props) => {
 
   return (
     <div>
+      {error}
       <FormInput name="email" type="text" value={email} handleChange={handleChange} label="email"></FormInput>
       <FormInput
         name="password"
@@ -43,6 +62,7 @@ const SignIn: VFC = (props) => {
         label="password"
       ></FormInput>
       <div>
+        <input type="submit" value="TestSubmit" onClick={(evt) => TestHandleSubmit(evt)} />
         <input type="submit" value="submit" onClick={(evt) => handleSubmit(evt)} />
       </div>
       <div>
