7be3bd5b43e99c3c5579e3f37fbf029d08e990ca
Koichi <koichi89592@gmail.com>
2022-02-28 18:38:28 -0800

    Set icons to completed todo items

--- src/components/completedTodoListItem/completedTodoListItem.tsx
+++ src/components/completedTodoListItem/completedTodoListItem.tsx
@@ -6,6 +6,19 @@ import { Todo } from 'redux/todo/todo.actions';
 import { updateTodosStartAsync } from 'redux/todo/todo.actions';
 import { formattedDateString } from 'utils';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import { IconLookup, IconDefinition, findIconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fas);
+
+const trashLookup: IconLookup = { prefix: 'fas', iconName: 'trash' };
+const trashIconDefinition: IconDefinition = findIconDefinition(trashLookup);
+const returnLookup: IconLookup = { prefix: 'fas', iconName: 'arrow-rotate-left' };
+const returnIconDefinition: IconDefinition = findIconDefinition(returnLookup);
+
 type CompletedTodoListItemProps = {
   todo: Todo;
   userId: string;
@@ -26,8 +39,13 @@ export const CompletedTodoListItem: VFC<CompletedTodoListItemProps> = ({ todo, u
       <span>
         {todo.description} - {formattedDateString(todo.dueDate)}
       </span>
-      <button onClick={() => onClickCheckBox(todo)}>Delete</button>
-      <button onClick={() => handlerOnClick(todo)}>Return</button>
+      <button onClick={() => onClickCheckBox(todo)}>
+        <FontAwesomeIcon icon={trashIconDefinition} />
+      </button>
+      <button onClick={() => handlerOnClick(todo)}>
+        {/* <FontAwesomeIcon icon="fa-solid fa-arrow-rotate-left" /> */}
+        <FontAwesomeIcon icon={returnIconDefinition} />
+      </button>
     </div>
   );
 };
